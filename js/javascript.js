52ac69a756d464ed2b445641ef4ad1a5d6264262
Leandro Bareiro <leanbareiro19@gmail.com>
2022-07-28 23:14:21 -0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 16; i++) {
+    let nombreCliente = prompt("Ingrese su nombre.");
+    let trabajo = prompt("Que se quiere hacer: Corte Clásico, Corte y Barba")
+    alert(" Turno N° "+i+" Nombre: "+ nombreCliente+ ", Trabajo: "+ trabajo)
+    alert("Gracias por elegirnos.")
+}
